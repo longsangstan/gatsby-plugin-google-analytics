57b1cd4e7944d21ad24399f0ed28ad8137053d82
Long Sang Chan <chancls@connect.hku.hk>
2018-03-14 23:16:18 +0800

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+"use strict";
+
+exports.onRouteUpdate = function(_ref) {
+    var location = _ref.location;
+    // Don't track while developing.
+    if (process.env.NODE_ENV === "production" && typeof ga === "function") {
+        window.ga(
+            "set",
+            "page",
+            location ? location.pathname + location.search : undefined
+        );
+        window.ga("send", "pageview");
+    }
+};
