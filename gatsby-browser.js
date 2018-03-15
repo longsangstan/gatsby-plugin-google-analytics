bbe94029ad5e6e0579adb4b7d052ac1b3bd7bb6c
Long Sang Chan <chancls@connect.hku.hk>
2018-03-16 05:08:54 +0800

    add hash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,14 @@
 "use strict";
 
 exports.onRouteUpdate = function(_ref) {
-    var location = _ref.location;
-    // Don't track while developing.
-    if (process.env.NODE_ENV === "production" && typeof ga === "function") {
-        window.ga(
-            "set",
-            "page",
-            location ? location.pathname + location.search : undefined
-        );
-        window.ga("send", "pageview");
-    }
+  var location = _ref.location;
+  // Don't track while developing.
+  if (process.env.NODE_ENV === "production" && typeof ga === "function") {
+    window.ga(
+      "set",
+      "page",
+      location ? location.pathname + location.search + location.hash : undefined
+    );
+    window.ga("send", "pageview");
+  }
 };
